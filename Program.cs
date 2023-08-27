4f3a58d70dc13cf69299b1303389442a05e25d15
Gizelle5 <142949027+Gizelle5@users.noreply.github.com>
2023-08-27 12:34:04 -0500

    Homework 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Week1Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double x = 2.5;
+        double y = 3.3;
+        double calculatex = (4*x*x);
+        double calculatey = (3*y);
+        double Z = calculatex + calculatey;
+        
+
+        Console.Write("x = " + x);
+        Console.WriteLine(", y = " + y);
+        Console.WriteLine("The value of Z is: " + Z);
+    }
+}
