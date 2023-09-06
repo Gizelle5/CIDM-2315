67c8d2b8e43626a8c19f91fe6bac692eb63fa914
Gizelle5 <142949027+Gizelle5@users.noreply.github.com>
2023-09-05 22:49:52 -0500

    Homework 2
    GPA Grade Point Converter

---
+++ Homework2_Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Week2Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a letter grade: ");
+        string letter_grade = Console.ReadLine();
+        int gpa;
+
+        switch (letter_grade)
+        {
+            //A
+        case "A":
+            gpa = 4;
+            Console.WriteLine("GPA point: " + gpa);
+            break;
+
+            //B
+        case "B":
+            gpa = 3; 
+            Console.WriteLine("GPA point: " + gpa);
+            break;
+
+            //C
+        case "C":
+            gpa = 2;
+            Console.WriteLine("GPA point: " + gpa);
+            break;
+
+            //D
+        case "D":
+            gpa = 1;
+            Console.WriteLine("GPA point: " + gpa);
+            break;
+
+            //F
+        case "F":
+            gpa = 0;
+            Console.WriteLine("GPA point: " + gpa);
+            break;
+
+            //wrong letter input
+        default:
+            Console.WriteLine("Wrong Letter Grade!");
+            break; 
+        }
+        
+    }
+}
