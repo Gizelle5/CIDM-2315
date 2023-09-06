c7bb2d722d553a0de4a5547d1281247196789003
Gizelle5 <142949027+Gizelle5@users.noreply.github.com>
2023-09-05 23:51:23 -0500

    Week 2 Homework Bonus
    Leap year checker

---
+++ Week2Bonus_Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Week2Bonus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a year: ");
+        string number = Console.ReadLine();
+        int year = Convert.ToInt16(number);
+
+        bool LeapYear = ((year%4 == 0)&&(year%400 == 0 || year%100 != 0));
+
+        //leap year
+        if (LeapYear)
+        { 
+            Console.WriteLine(year + " is a Leap Year.");
+        }
+        // not leap year
+        else
+        {
+            Console.WriteLine(year + " is not a Leap Year.");
+        }
+    }
+}
