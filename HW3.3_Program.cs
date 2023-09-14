04b397f3e7b152bdb460306f4b4994fe661e1df2
Gizelle5 <142949027+Gizelle5@users.noreply.github.com>
2023-09-13 23:01:51 -0500

    Homework 3 (3 questions)

---
+++ HW3.3_Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework3._3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Assign an integer value to N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+
+    for(int row = 0; row<=N; row++){
+         for(int col = 0; col<=N; col++)
+    {
+        if(row>col)
+
+         Console.Write("*");
+         
+    }
+    Console.WriteLine("");
+}
+
+    }
+}
---
+++ HW3_ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week3Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input an integer: ");
+        int Num = Convert.ToInt16(Console.ReadLine());
+
+        if (Num < 2){
+                Console.WriteLine("N is non-prime");
+        }
+        else{
+            for(int N = 2; N < Num; N++){
+                if (Num % N == 0){
+                    Console.WriteLine("N is non-prime");
+                    break;
+            }  
+                else{
+                Console.WriteLine("N is prime");
+                break;
+            }
+        }
+            
+        }
+    }
+}
